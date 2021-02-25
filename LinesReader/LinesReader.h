3844df80136ccccc8a43b8f05388cd01929fd249
d000b <makc.Hitman@gmail.com>
2021-02-25 20:10:25 +0800

    Update LinesReader.h

--- LinesReader/LinesReader.h
+++ LinesReader/LinesReader.h
@@ -6,7 +6,8 @@
 
 namespace UltimaAPI
 {
-	bool ut8toWide(std::wstring&, std::string&);
+	bool  ut8toWide(std::wstring&, std::string&);
+	bool  ut8toWide(std::wstring& to, char* from);
 	bool  fread(const wchar_t*, std::vector<std::wstring>&);
 };
 
@@ -24,17 +25,33 @@ bool  UltimaAPI::ut8toWide(std::wstring& to, std::string& from)
 	return !!b;
 }
 
+bool  UltimaAPI::ut8toWide(std::wstring& to, char* from)
+{
+	auto len = strlen(from);
+	auto i = MultiByteToWideChar(CP_UTF8, 0, from, len, NULL, 0);
+	LPWSTR wch = new WCHAR[i + 1];
+	int b;
+	if (b = MultiByteToWideChar(CP_UTF8, 0, from, len, wch, i))
+	{
+		wch[i] = 0;
+		to = wch;
+	}
+	delete[] wch;
+	return !!b;
+}
+
 bool  UltimaAPI::fread(const wchar_t* name, std::vector<std::wstring>& wcs)
 {
-	std::ifstream f(name, std::ios::out);
-	if (f.is_open())
+	FILE* f;
+	_wfopen_s(&f, name, L"r");
+	if (f)
 	{
-		std::string str;
+		char b[256];
 		std::wstring wc;
-		while (std::getline(f, str))
-			if (ut8toWide(wc, str))
+		while (fgets(b, sizeof(b), f))
+			if (ut8toWide(wc, b))
 				wcs.push_back(wc);
-		f.close();
+		fclose(f);
 		return true;
 	}
 	else return false;
