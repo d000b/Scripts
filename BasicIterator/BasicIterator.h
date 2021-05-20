d6a02dd0f82796f64029809734c98be7e23f46dc
d000b <makc.Hitman@gmail.com>
2021-05-20 08:39:04 +0800

    Update BasicIterator.h

--- BasicIterator/BasicIterator.h
+++ BasicIterator/BasicIterator.h
@@ -4,32 +4,22 @@
 
 namespace UltimaAPI
 {
-	template <typename type>
-	class Vector;
-	template <typename type>
-	class BasicString;
+	template <typename type>  class Vector;
 
-	template <typename type>
-	class BasicIterator;
+	template <typename type, typename maps>  class PseudoTensor;
+
+	template <typename type>  class BasicIterator;
 }
 
 template <typename type>
 class  UltimaAPI::BasicIterator : public std::iterator<std::input_iterator_tag, type>
 {
 	extern friend class UltimaAPI::Vector<type>;
-	extern friend class UltimaAPI::BasicString<type>;
 
 	type* pointer;
-private:
-	BasicIterator(type* p) : pointer(p)
-	{
-
-	}
 public:
-	BasicIterator(const BasicIterator& it) : pointer(it.p)
-	{
-
-	}
+	BasicIterator(type* p) : pointer(p) { }
+	BasicIterator(const BasicIterator& it) : pointer(it.p) { }
 
 	bool operator!=(BasicIterator const& it) const
 	{
