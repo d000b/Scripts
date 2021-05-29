a223143d4ac116243bf77c7736696bf16b847603
d000b <makc.Hitman@gmail.com>
2021-05-29 10:09:16 +0800

    Update util.h

--- Memory/util.h
+++ Memory/util.h
@@ -5,19 +5,33 @@ namespace UltimaAPI
 	namespace Memory
 	{
 		template <typename type>  bool memset(void*, void*, type);
+		template <typename type>  bool memcpy(type*, type*, size_t);
 	}
 }
 
 template <typename type>
-bool UltimaAPI::Memory::memset(void* begin, void* end, type value)
+bool	UltimaAPI::Memory::memset(void* begin, void* end, type value)
 {
 	if (begin > end)
 		return false;
 	while (begin != end)
-	{
-		*static_cast<type*>(begin) = value;
-		++begin;
-	}
+		*static_cast<type*>(begin++) = value;
 	return true;
 }
 
+template <typename type>
+bool	UltimaAPI::Memory::memcpy(type* to, type* from, size_t count)
+{
+	if (from == to)
+		return true;
+	if (count > 1)
+	{
+		if (from < to && from + count >= to)
+			for (size_t i = count - 1; i != -1; i--)
+				to[i] = from[i];
+		else for (size_t i = 0; i < count; i++)
+				to[i] = from[i];
+	}
+	else *to = *from;
+	return true;
+}
